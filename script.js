453e9e06cedd29df2bc7fea9d017500af757b8c6
Michael Wiltfong <76107997+mdwiltfong@users.noreply.github.com>
2021-08-28 04:18:43 -0400

    First Task

---
+++ script.js
@@ -0,0 +1,5 @@
+// When the DOM is ready, console.log the message “Let’s get ready to party with jQuery!”
+
+$('document').ready(function (){
+    console.log(`Let's get ready to party with jQuery`)
+})
