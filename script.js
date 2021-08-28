c84339b57b86aa8f66927a25c486287c6173c636
Michael Wiltfong <76107997+mdwiltfong@users.noreply.github.com>
2021-08-28 05:12:05 -0400

    Add to list

--- script.js
+++ script.js
@@ -3,3 +3,19 @@
 $('document').ready(function (){
     console.log(`Let's get ready to party with jQuery`)
 })
+
+/*Give all images inside of an article tag the 
+class of image-center (this class is defined inside of the style tag in the head). */
+
+$('article img').addClass('image-center')
+
+//Remove the last paragraph in the article
+
+$('article p:last').remove();
+
+//Set the font size of the title to be a random pixel size from 0 to 100.
+$('#title').css('font-size',`${Math.floor(Math.random()*100)}`)
+
+//Add an item to the list; it can say whatever you want.
+
+$('ol').append('<li>The puppy is also in a cup! WOW</li>')
